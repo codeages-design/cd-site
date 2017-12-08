b8c6d0deb7822d6dc58e3e55a3f8a90a4cbff689
benzlee <benzlee@yeah.net>
2017-12-08 15:05:41 +0800

    modified sidebar style&text

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -1,24 +1,24 @@
 export const navData = [
   {
     isItem: false,
-    text: '基础'
+    text: '基础 Basic'
   },
   {
     isItem: true,
-    // text_en: 'Color',
     text_zh: '色彩',
+    text_en: 'Color',
     name: 'component_color'
   },
   {
     isItem: true,
-    // text_en: 'Font',
     text_zh: '字体',
+    text_en: 'Font',
     name: 'component_font'
   },
   {
     isItem: true,
-    // text_en: 'Typography',
     text_zh: '排版',
+    text_en: 'Typography',
     name: 'component_typo'
   },
   // {
@@ -29,18 +29,18 @@ export const navData = [
   // },
   {
     isItem: false,
-    text: '通用'
+    text: '通用 Common'
   },
   {
     isItem: true,
-    // text_en: 'Panel',
     text_zh: '面板',
+    text_en: 'Panel',
     name: 'component_panel'
   },
   {
     isItem: true,
-    // text_en: 'Card',
     text_zh: '卡片',
+    text_en: 'Card',
     name: 'component_card'
   },
   // {
@@ -51,161 +51,161 @@ export const navData = [
   // },
   {
     isItem: true,
-    // text_en: 'Button',
     text_zh: '按钮',
+    text_en: 'Button',
     name: 'component_button'
   },
   {
     isItem: true,
-    // text_en: 'Tag',
     text_zh: '标签',
+    text_en: 'Tag',
     name: 'component_tag'
   },
   {
     isItem: true,
-    // text_en: 'Status',
     text_zh: '状态',
+    text_en: 'Status',
     name: 'component_status'
   },
   {
     isItem: true,
-    // text_en: 'Avatar',
     text_zh: '头像',
+    text_en: 'Avatar',
     name: 'component_avatar'
   },
   
   {
     isItem: false,
-    text: '表单'
+    text: '表单 Form'
   },
   {
     isItem: true,
-    // text_en: 'Input',
     text_zh: '输入框',
+    text_en: 'Input',
     name: 'component_input'
   },
   {
     isItem: true,
-    // text_en: 'Radio',
     text_zh: '单选框',
+    text_en: 'Radio',
     name: 'component_radio'
   },
   {
     isItem: true,
-    // text_en: 'Checkbox',
     text_zh: '多选框',
+    text_en: 'Checkbox',
     name: 'component_checkbox'
   },
   {
     isItem: true,
-    // text_en: 'Select',
     text_zh: '选择框',
+    text_en: 'Select',
     name: 'component_select'
   },
   {
     isItem: true,
-    // text_en: 'Switch',
     text_zh: '开关',
+    text_en: 'Switch',
     name: 'component_switch'
   },
   {
     isItem: false,
-    text: '通知'
+    text: '通知 Notice'
   },
   {
     isItem: true,
-    // text_en: 'Alert',
     text_zh: '提示框',
+    text_en: 'Alert',
     name: 'component_alert'
   },
   {
     isItem: true,
-    // text_en: 'Confirm',
     text_zh: '确认框',
+    text_en: 'Confirm',
     name: 'component_confirm'
   },
   {
     isItem: true,
-    // text_en: 'Message',
     text_zh: '信息框',
+    text_en: 'Message',
     name: 'component_message'
   },
   {
     isItem: true,
-    // text_en: 'Feedback',
     text_zh: '反馈',
+    text_en: 'Feedback',
     name: 'component_feedback'
   },
   {
     isItem: true,
-    // text_en: 'Progress',
     text_zh: '进度条',
+    text_en: 'Progress',
     name: 'component_progress'
   },
   {
     isItem: false,
-    text: '导航'
+    text: '导航 Navigation'
   },
   {
     isItem: true,
-    // text_en: 'Tabs',
     text_zh: '标签页',
+    text_en: 'Tabs',
     name: 'component_tabs'
   },
   {
     isItem: true,
-    // text_en: 'Steps',
     text_zh: '步骤条',
+    text_en: 'Steps',
     name: 'component_steps'
   },
   {
     isItem: true,
-    // text_en: 'Pagination',
     text_zh: '分页',
+    text_en: 'Pagination',
     name: 'component_pagination'
   },
   {
     isItem: false,
-    text: '提示'
+    text: '提示 Hint'
   },
   {
     isItem: true,
-    // text_en: 'Tooltip',
     text_zh: '文字提示',
+    text_en: 'Tooltip',
     name: 'component_tooltip'
   },
   {
     isItem: true,
-    // text_en: 'Badge',
     text_zh: '徽章',
+    text_en: 'Badge',
     name: 'component_badge'
   },
   {
     isItem: true,
-    // text_en: 'Security',
     text_zh: '安全性',
+    text_en: 'Security',
     name: 'component_security'
   },
   {
     isItem: true,
-    // text_en: 'Loading',
     text_zh: '加载中',
+    text_en: 'Loading',
     name: 'component_loading'
   },
   {
     isItem: false,
-    text: '数据'
+    text: '数据 Data'
   },
   {
     isItem: true,
-    // text_en: 'Setting',
     text_zh: '设置',
+    text_en: 'Setting',
     name: 'component_setting'
   },
   {
     isItem: true,
-    // text_en: 'Table',
     text_zh: '表格',
+    text_en: 'Table',
     name: 'component_table'
   },
   // {
