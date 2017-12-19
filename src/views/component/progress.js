99cf76d3132e0aba80481935b92ebc2025c027d6
benzlee <benzlee@yeah.net>
2017-12-19 17:26:13 +0800

    modified progress page

--- src/views/component/progress.js
+++ src/views/component/progress.js
@@ -1,8 +1,35 @@
 export const progress_base = `
-<div class="cd-progress cd-progress-info">
+<div class="cd-progress">
   <div class="progress-bar">
     <div class="progress-outer">
       <div class="progress-inner" style="width: 50%;"></div> 
+    </div>
+  </div>
+  <div class="progress-text">50%</div>
+</div>
+<div class="cd-progress cd-progress-success">
+  <div class="progress-bar">
+    <div class="progress-outer">
+      <div class="progress-inner" style="width: 100%;"></div>
+    </div>
+  </div>
+  <div class="progress-text"><i class="cd-icon cd-icon-success cd-text-sm cd-text-success"></i></div>
+</div>
+<div class="cd-progress cd-progress-danger">
+  <div class="progress-bar">
+    <div class="progress-outer">
+      <div class="progress-inner" style="width: 78%;"></div>
+    </div>
+  </div>
+  <div class="progress-text"><i class="cd-icon cd-icon-danger cd-text-sm cd-text-danger"></i></div>
+</div>
+`;
+
+export const progress_active = `
+<div class="cd-progress">
+  <div class="progress-bar">
+    <div class="progress-outer">
+      <div class="progress-inner active" style="width: 50%;"></div> 
     </div>
   </div>
   <div class="progress-text">50%</div>
@@ -10,36 +37,7 @@ export const progress_base = `
 `;
 
 export const progress_size = `
-<div class="cd-progress cd-progress-info cd-progress-sm">
-  <div class="progress-bar">
-    <div class="progress-outer">
-      <div class="progress-inner" style="width: 50%;"></div> 
-    </div>
-  </div>
-  <div class="progress-text">50%</div>
-</div>
-
-<div class="cd-progress cd-progress-info">
-  <div class="progress-bar">
-    <div class="progress-outer">
-      <div class="progress-inner" style="width: 50%;"></div> 
-    </div>
-  </div>
-  <div class="progress-text">50%</div>
-</div>
-
-<div class="cd-progress cd-progress-info cd-progress-lg">
-  <div class="progress-bar">
-    <div class="progress-outer">
-      <div class="progress-inner" style="width: 50%;"></div> 
-    </div>
-  </div>
-  <div class="progress-text">50%</div>
-</div>
-`;
-
-export const progress_color = `
-<div class="cd-progress cd-progress-danger">
+<div class="cd-progress cd-progress-sm">
   <div class="progress-bar">
     <div class="progress-outer">
       <div class="progress-inner" style="width: 25%;"></div> 
@@ -48,7 +46,7 @@ export const progress_color = `
   <div class="progress-text">25%</div>
 </div>
 
-<div class="cd-progress cd-progress-warning">
+<div class="cd-progress">
   <div class="progress-bar">
     <div class="progress-outer">
       <div class="progress-inner" style="width: 50%;"></div> 
@@ -57,21 +55,12 @@ export const progress_color = `
   <div class="progress-text">50%</div>
 </div>
 
-<div class="cd-progress cd-progress-info">
+<div class="cd-progress cd-progress-lg">
   <div class="progress-bar">
     <div class="progress-outer">
-      <div class="progress-inner" style="width: 75%;"></div>
+      <div class="progress-inner" style="width: 75%;"></div> 
     </div>
   </div>
   <div class="progress-text">75%</div>
-</div>
-
-<div class="cd-progress cd-progress-success">
-  <div class="progress-bar">
-    <div class="progress-outer">
-      <div class="progress-inner" style="width: 100%;"></div>
-    </div>
-  </div>
-  <div class="progress-text"><i class="cd-icon cd-icon-success cd-text-sm cd-text-success"></i></div>
 </div>
 `;
