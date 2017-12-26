635ee3e1fcf8c02b8c234850d9ba26a8078b45c0
benzlee <benzlee@yeah.net>
2017-12-26 16:17:54 +0800

    alert page modified

--- src/views/component/Alert.vue
+++ src/views/component/Alert.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <div class="site-content-title">提示框</div>
-    <div class="site-content-des">提示框用于开始一个即时操作</div>
+    <div class="site-content-des">提示框的作用是在页面级别的交互反馈中用以给用户展示场景信息，大部分情况下，提示框都应该是固定在某个具体位置不主动消失。</div>
     <div class="cd-row">
       <div class="col-xs-6">
         <x-panel :code="code.alert_color|trim">
           <div slot="title">
-            提示框颜色
+            场景提示框
           </div>
           <div slot="code">
             <pre v-highlightjs><code class="html">
@@ -15,14 +15,14 @@
           </div>
           <div class="cd-mb16" slot="style" v-html="code.alert_color"></div>
           <div class="cd-text-xs" slot="dec">
-            针对提示框颜色的描述
+            我们将场景色引入了提示框中，为的是让用户通过场景色能够快速感知提示框可能带来的内容定义。通过成功提示框、错误提示框、警告提示框、信息提示框来区分不同场景下的信息提示。
           </div>
         </x-panel>
       </div>
       <div class="col-xs-6">
         <x-panel :code="code.alert_close|trim">
           <div slot="title">
-            可关闭的提示框
+            可关闭提示框
           </div>
           <div slot="code">
             <pre v-highlightjs><code class="html">
@@ -32,7 +32,7 @@
           <div class="cd-mb16" slot="style" v-html="code.alert_close">
           </div>
           <div class="cd-text-xs" slot="dec">
-            针对可关闭的提示框的描述
+            在一些偏向营销的提示框场景下，我们应该给用户以关闭“碍眼的提示信息”的权利。
           </div>
         </x-panel>
       </div>
