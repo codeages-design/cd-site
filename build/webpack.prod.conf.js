ac2cbde85ba2a70aa0a3d034f35771dfa3a89cce
zhongjie <zhongjie@howzhi.com>
2018-01-23 09:39:35 +0800

    修改webpack 配置

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -15,7 +15,8 @@ var webpackConfig = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({
       sourceMap: config.build.productionSourceMap,
-      extract: true
+      // extract: true
+      extract: false
     })
   },
   devtool: config.build.productionSourceMap ? '#source-map' : false,
