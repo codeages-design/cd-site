c5a7325960c61da6177a8ca1b9e086f98770168d
zhongjie <zhongjie@howzhi.com>
2018-04-03 11:56:01 +0800

    集成ts

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -10,7 +10,7 @@ function resolve (dir) {
 
 var baseWebpackConfig = {
   entry: {
-    app: './src/main.js'
+    app: './src/main.ts'
   },
   output: {
     path: config.build.assetsRoot,
@@ -20,7 +20,7 @@ var baseWebpackConfig = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.js', '.vue', '.json', '.ts', '.tsx'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src')
@@ -32,6 +32,14 @@ var baseWebpackConfig = {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        }
       },
       {
         test: /\.js$/,
