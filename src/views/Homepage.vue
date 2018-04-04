e9cf864e448efff7c295f3d585912814b41ef243
zhongjie <zhongjie@howzhi.com>
2018-04-04 11:13:53 +0800

    首页改造成ts

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -9,88 +9,27 @@
           <button class="cd-btn cd-btn-primary cd-btn-lg" @click="getMore">了解更多</button>
         </div>
         <div class="site-homepage-banner__pic" 
-          :style="{transform: `perspective(${banner.perspective}px) rotateX(${banner.rotateX}deg) rotateY(${banner.rotateY}deg)`}" 
+          :style="{transform: `perspective(${banner.global.perspective}px) rotateX(${banner.global.rotateY}deg) rotateY(${banner.global.rotateX}deg)`}" 
           @mousemove="mousemove" 
           @mouseleave="mouseleave" 
           ref="banner">
-          <img :style="{transform: `matrix(1, 0, 0, 1, ${banner.boss.rotateX}, ${banner.boss.rotateY}`}"
-            :class="{active: isActive, prohibit: isProhibit}" 
-            class="site-homepage-banner__boss" 
-            src="/static/img/homepage/boss.png" 
-            srcset="/static/img/homepage/boss@2x.png 2x" alt="">
-          <img :style="{transform: `matrix(1, 0, 0, 1, ${banner.designer.rotateX}, ${banner.designer.rotateY}`}"
-            :class="{active: isActive, prohibit: isProhibit}" 
-            class="site-homepage-banner__designer" 
-            src="/static/img/homepage/designer.png" 
-            srcset="/static/img/homepage/designer@2x.png 2x" alt="">
-          <img :style="{transform: `matrix(1, 0, 0, 1, ${banner.engineer.rotateX}, ${banner.engineer.rotateY}`}"
-            :class="{active: isActive, prohibit: isProhibit}" 
-            class="site-homepage-banner__engineer" 
-            src="/static/img/homepage/engineer.png" 
-            srcset="/static/img/homepage/engineer@2x.png 2x" alt="">
-          <img :style="{transform: `matrix(1, 0, 0, 1, ${banner.component.rotateX}, ${banner.component.rotateY}`}"
-            :class="{active: isActive, prohibit: isProhibit}" 
-            class="site-homepage-banner__component" 
-            src="/static/img/homepage/component.png" 
-            srcset="/static/img/homepage/component@2x.png 2x" alt="">
-          <img :style="{transform: `matrix(1, 0, 0, 1, ${banner.vase.rotateX}, ${banner.vase.rotateY}`}"
-            :class="{active: isActive, prohibit: isProhibit}" 
-            class="site-homepage-banner__vase" 
-            src="/static/img/homepage/vase.png" 
-            srcset="/static/img/homepage/vase@2x.png 2x" alt="">
-          <img :style="{transform: `matrix(1, 0, 0, 1, ${banner.computer.rotateX}, ${banner.computer.rotateY}`}"
-            :class="{active: isActive, prohibit: isProhibit}" 
-            class="site-homepage-banner__computer" 
-            src="/static/img/homepage/computer.png" 
-            srcset="/static/img/homepage/computer@2x.png 2x" alt="">
-          <img :style="{transform: `matrix(1, 0, 0, 1, ${banner.desktop.rotateX}, ${banner.desktop.rotateY}`}"
-            :class="{active: isActive, prohibit: isProhibit}" 
-            class="site-homepage-banner__desktop" 
-            src="/static/img/homepage/desktop.png" 
-            srcset="/static/img/homepage/desktop@2x.png 2x" alt="">
+          <img :style="{transform: `matrix(1, 0, 0, 1, ${item.rotateX}, ${item.rotateY}`}"
+            :class="[{active: isActive, prohibit: isProhibit}, `site-homepage-banner__${index}`]" 
+            :src="item.src"
+            :srcset="item.srcset" alt="" v-for="(item, index) in banner" :key="index" :data-a="banner[item]" v-if="index != 'global'">
         </div>
       </section>
       <section class="site-homepage-feature">
-        <div class="site-homepage-feature__item">
+        <div class="site-homepage-feature__item" :class="{middle: index == 1}" v-for="(item, index) in feature" :key="index">
           <div class="site-homepage-feature__thumb">
-            <img src="/static/img/homepage/feature_1.png" 
-              srcset="/static/img/homepage/feature_1@2x.png 2x" alt="">
+            <img :src="item.src" 
+              :srcset="item.srcset" alt="">
           </div>
           <div class="site-homepage-feature__title">
-            帮助产品经理搭建原型
+            {{ item.title }}
           </div>
           <div class="site-homepage-feature__content">
-            了解设计指南，帮助产品经理搭建逻辑清晰，结构合理且高效易用的产品。
-          </div>
-          <div class="site-homepage-feature__action">
-            <button class="cd-btn cd-btn-primary cd-btn-lg" @click="getMore">查看详情</button>
-          </div>
-        </div>
-        <div class="site-homepage-feature__item" style="margin: 0 180px;">
-          <div class="site-homepage-feature__thumb">
-            <img src="/static/img/homepage/feature_2.png" 
-              srcset="/static/img/homepage/feature_2@2x.png 2x" alt="">
-          </div>
-          <div class="site-homepage-feature__title">
-            帮助开发复用代码和组件元素
-          </div>
-          <div class="site-homepage-feature__content">
-            使用组件演示快速体验交互细节，使用前端框架封装的代码帮助快速开发。
-          </div>
-          <div class="site-homepage-feature__action">
-            <button class="cd-btn cd-btn-primary cd-btn-lg" @click="getMore">查看详情</button>
-          </div>
-        </div>
-        <div class="site-homepage-feature__item">
-          <div class="site-homepage-feature__thumb">
-            <img src="/static/img/homepage/feature_3.png" 
-              srcset="/static/img/homepage/feature_3@2x.png 2x" alt="">
-          </div>
-          <div class="site-homepage-feature__title">
-            帮助设计减少重复劳动力
-          </div>
-          <div class="site-homepage-feature__content">
-            下载相关资源快速搭建页面原型或高保真视觉稿，提升产品设计效率。
+            {{ item.content }}
           </div>
           <div class="site-homepage-feature__action">
             <button class="cd-btn cd-btn-primary cd-btn-lg" @click="getMore">查看详情</button>
@@ -112,63 +51,104 @@
   </div>
 </template>
 
-<script>
-import XHeader from '@/views/Header';
+<script lang="ts">
+import Vue from 'vue';
+import { Component, } from 'vue-property-decorator';
+
+import XHeader from '@/views/Header.vue';
 import { version } from 'codeages-design/package.json';
- 
-export default {
+
+@Component({
   name: 'homepage',
   components: {
-    XHeader,
-  },
-  data() {
-    return {
-      version,
-      banner: {
-        rotateX: 0,
-        rotateY: 0,
-        perspective: 250,
-        ratio: 1.5,
-        boss: {
-          rotateX: 0,
-          rotateY: 0,
-          ratio: -3,
-        },
-        designer: {
-          rotateX: 0,
-          rotateY: 0,
-          ratio: 4,
-        },
-        engineer: {
-          rotateX: 0,
-          rotateY: 0,
-          ratio: 4,
-        },
-        component: {
-          rotateX: 0,
-          rotateY: 0,
-          ratio: 2,
-        },
-        vase: {
-          rotateX: 0,
-          rotateY: 0,
-          ratio: -3,
-        },
-        computer: {
-          rotateX: 0,
-          rotateY: 0,
-          ratio: 3,
-        },
-        desktop: {
-          rotateX: 0,
-          rotateY: 0,
-          ratio: 1,
-        },
-      },
-      isProhibit: false,
-      isActive: false
-    }
-  },
+    XHeader
+  }
+})
+export default class extends Vue {
+  // data
+  version: string = version;
+  banner: any = {
+    global: {
+      rotateX: 0,
+      rotateY: 0,
+      perspective: 250,
+      ratio: 1.5,
+    },
+    boss: {
+      rotateX: 0,
+      rotateY: 0,
+      ratio: -3,
+      src: '/static/img/homepage/boss.png',
+      srcset: '/static/img/homepage/boss@2x.png 2x'
+    },
+    designer: {
+      rotateX: 0,
+      rotateY: 0,
+      ratio: 4,
+      src: '/static/img/homepage/designer.png',
+      srcset: '/static/img/homepage/designer@2x.png 2x'
+    },
+    engineer: {
+      rotateX: 0,
+      rotateY: 0,
+      ratio: 4,
+      src: '/static/img/homepage/engineer.png',
+      srcset: '/static/img/homepage/engineer@2x.png 2x'
+    },
+    component: {
+      rotateX: 0,
+      rotateY: 0,
+      ratio: 2,
+      src: '/static/img/homepage/component.png',
+      srcset: '/static/img/homepage/component@2x.png 2x'
+    },
+    vase: {
+      rotateX: 0,
+      rotateY: 0,
+      ratio: -3,
+      src: '/static/img/homepage/vase.png',
+      srcset: '/static/img/homepage/vase@2x.png 2x'
+    },
+    computer: {
+      rotateX: 0,
+      rotateY: 0,
+      ratio: 3,
+      src: '/static/img/homepage/computer.png',
+      srcset: '/static/img/homepage/computer@2x.png 2x'
+    },
+    desktop: {
+      rotateX: 0,
+      rotateY: 0,
+      ratio: 1,
+      src: '/static/img/homepage/desktop.png',
+      srcset: '/static/img/homepage/desktop@2x.png 2x'
+    },
+  };
+  
+  feature: any = [
+    {
+      title: '帮助产品经理搭建原型',
+      content: '了解设计指南，帮助产品经理搭建逻辑清晰，结构合理且高效易用的产品。',
+      src: '/static/img/homepage/feature_1.png',
+      srcset: "/static/img/homepage/feature_1@2x.png 2x"
+    },
+    {
+      title: '帮助开发复用代码和组件元素',
+      content: '使用组件演示快速体验交互细节，使用前端框架封装的代码帮助快速开发。',
+      src: '/static/img/homepage/feature_2.png',
+      srcset: "/static/img/homepage/feature_2@2x.png 2x"
+    },
+    {
+      title: '帮助设计减少重复劳动力',
+      content: '下载相关资源快速搭建页面原型或高保真视觉稿，提升产品设计效率。',
+      src: '/static/img/homepage/feature_3.png',
+      srcset: "/static/img/homepage/feature_3@2x.png 2x"
+    },
+  ];
+
+  isProhibit: boolean = false;
+  isActive: boolean = false
+
   created() {
     setTimeout(() => {
       this.isActive = true
@@ -177,62 +157,44 @@ export default {
     setTimeout(() => {
       this.isProhibit = true;
     }, 1400)
-  },
-  methods: {
-    mousemove(event) {
-      const e = event || window.event;
-      const rect = this.$refs['banner'].getBoundingClientRect();
+  }
 
-      const middlePoint = {
-        x: parseInt(rect.x + rect.width / 2),
-        y: parseInt(rect.y + rect.height / 2)
-      };
+  mousemove(event) {
+    const e = event || window.event;
+    const bannerEl: any = this.$refs['banner'];
+    const rect = bannerEl.getBoundingClientRect();
 
-      const mouse = {
-        x: e.clientX,
-        y: e.clientY
-      };
+    const middlePoint= {
+      x: parseInt(rect.x + rect.width / 2),
+      y: parseInt(rect.y + rect.height / 2)
+    };
 
-      function getRotateX(ratio) {
-        return parseFloat((middlePoint.x - mouse.x) / (middlePoint.x - rect.x) * ratio).toFixed(2);
-      }
+    const mouse = {
+      x: parseFloat(e.clientX),
+      y: parseFloat(e.clientY)
+    };
 
-      function getRotateY(ratio) {
-        return parseFloat((mouse.y - middlePoint.y) / (middlePoint.y - rect.y) * ratio).toFixed(2);
-      }
-
-      this.banner.rotateX = getRotateY(this.banner.ratio);
-      this.banner.rotateY = getRotateX(this.banner.ratio);
-
-      this.banner.boss.rotateX = getRotateX(this.banner.boss.ratio);
-      this.banner.boss.rotateY = getRotateY(this.banner.boss.ratio);
-
-      this.banner.designer.rotateX = getRotateX(this.banner.designer.ratio);
-      this.banner.designer.rotateY = getRotateY(this.banner.designer.ratio);
-
-      this.banner.engineer.rotateX = getRotateX(this.banner.engineer.ratio);
-      this.banner.engineer.rotateY = getRotateY(this.banner.engineer.ratio);
-
-      this.banner.component.rotateX = getRotateX(this.banner.component.ratio);
-      this.banner.component.rotateY = getRotateY(this.banner.component.ratio);
-
-      this.banner.vase.rotateX = getRotateX(this.banner.vase.ratio);
-      this.banner.vase.rotateY = getRotateY(this.banner.vase.ratio);
-
-      this.banner.computer.rotateX = getRotateX(this.banner.computer.ratio);
-      this.banner.computer.rotateY = getRotateY(this.banner.computer.ratio);
-
-      this.banner.desktop.rotateX = getRotateX(this.banner.desktop.ratio);
-      this.banner.desktop.rotateY = getRotateY(this.banner.desktop.ratio);
-
-    },
-    mouseleave() {
-      this.banner.rotateX = 0;
-      this.banner.rotateY = 0;
-    },
-    getMore() {
-      this.$router.push({ 'name': 'component' })
+    function getRotateX(ratio: number) {
+      return ((middlePoint.x - mouse.x) / (middlePoint.x - rect.x) * ratio).toFixed(2);
     }
+
+    function getRotateY(ratio: number) {
+      return ((mouse.y - middlePoint.y) / (middlePoint.y - rect.y) * ratio).toFixed(2);
+    }
+
+    Object.keys(this.banner).map((item) => {
+      this.banner[item].rotateX = getRotateX(this.banner[item].ratio);
+      this.banner[item].rotateY = getRotateY(this.banner[item].ratio);
+    })
+  }
+
+  mouseleave() {
+    this.banner.global.rotateX = 0;
+    this.banner.global.rotateY = 0;
+  }
+
+  getMore() {
+    this['$router'].push({ 'name': 'component' });
   }
 }
 </script>
