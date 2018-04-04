43040bbb56f81927eb515554813cfd1964e85ad4
zhongjie <zhongjie@howzhi.com>
2018-04-04 11:16:45 +0800

    add flie

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import MetaInfo from 'vue-meta-info';
+import App from '@/App.vue';
+import router from '@/router';
+import filters from '@/filters';
+import utils from '@/utils';
+
+import 'babel-polyfill';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/googlecode.css';
+import 'jquery';
+import 'codeages-design/dist/codeages-design';
+import 'codeages-design/src/less/codeages-design.less';
+
+Vue.use(MetaInfo);
+Vue.use(filters);
+Vue.use(utils);
+
+Vue.config.productionTip = false;
+
+Vue.directive('highlightjs', function(el) {
+  hljs.highlightBlock(el);
+});
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App },
+});
---
+++ src/vue-shims.d.ts
@@ -0,0 +1,6 @@
+declare module '*.vue' {
+  import Vue from 'vue';
+  export default Vue;
+}
+
+declare module 'codeages-design/package.json';
